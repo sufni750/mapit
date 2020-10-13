d291c9e32e5e1253adc85b442bd75c33b5b78e84
sufni750 <sufni750@gmail.com>
2020-10-13 14:11:48 +0200

    added command line and clipboard option for address

--- mapit.py
+++ mapit.py
@@ -1,3 +1,12 @@
-import webbrowser
-webbrowser.open('inventwithpython.com/')
+#! python3
+# mapit.py lanching a browser tab with map with adress from clipboard or command line
 
+import webbrowser, sys
+if len(sys.argv) > 1:
+	# Get address from command line
+	address = ' '.join(sys.argv[1:])
+else:
+	# Get address from clipboard
+	address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
